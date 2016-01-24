97e025808767a3d34c0ef7674392bb0f1c38fec2
Damir Svrtan <damir.svrtan@gmail.com>
2016-01-24 01:45:58 +0100

    Add better SQl select query after droping the table

--- lib/trespass/injection_matcher.rb
+++ lib/trespass/injection_matcher.rb
@@ -4,7 +4,7 @@ ALWAYS_PRESENT_TABLE = :schema_migrations
 
 RSpec::Matchers.define :be_sql_safe do
   match do |service|
-    user_input = "1%'; DROP TABLE #{ALWAYS_PRESENT_TABLE}; SELECT * FROM users WHERE name = '"
+    user_input = "1%'; DROP TABLE #{ALWAYS_PRESENT_TABLE}; SELECT * FROM information_schema.tables WHERE table_type = '"
     service.call(user_input)
     ActiveRecord::Base.connection.table_exists?('schema_migrations')
   end
